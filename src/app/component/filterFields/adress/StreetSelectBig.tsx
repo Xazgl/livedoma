4ea6e42b042dca901c6233672703fb28b5e73a13
Iam <lebezkavillage@gmail.com>
2024-11-07 18:58:15 +0300

    Тестовый новый фильтр

---
+++ src/app/component/filterFields/adress/StreetSelectBig.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { Autocomplete, TextField, AutocompleteRenderInputParams, } from "@mui/material";
+import { FilteblackProps, FilterUserOptions } from "../../../../../@types/dto";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import SignpostIcon from '@mui/icons-material/Signpost';
+import { useTheme } from "../../provider/ThemeProvider";
+
+type Props = {
+    filteblackProps: FilteblackProps;
+    currentFilter: FilterUserOptions;
+    setCurrentFilter: Dispatch<SetStateAction<FilterUserOptions>>;
+    resetPageAndReloadData:() => void;
+};
+
+export function StreetSelectBig({ filteblackProps, currentFilter, setCurrentFilter, resetPageAndReloadData }: Props) {
+    const { theme } = useTheme();
+    const [inputValue, setInputValue] = useState('');
+
+
+    const handleChange = (
+        event: React.ChangeEvent<{}>,
+        value: string | null
+    ) => {
+        setCurrentFilter((prevFilterState) => ({
+            ...prevFilterState,
+            street: value ? [value] : undefined,
+        }));
+        resetPageAndReloadData();
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<{}>, value: string) => {
+        setInputValue(value);
+    };
+
+    const filterOptions = (
+        options: string[],
+        { inputValue }: { inputValue: string }
+    ) => {
+        const filtered = options.filter(
+            (option) =>
+                option.toLowerCase().includes(inputValue.toLowerCase()) &&
+                inputValue.length >= 3
+        );
+        if (inputValue && !filtered.includes(inputValue)) {
+            filtered.unshift(inputValue);
+        }
+        return filtered;
+    };
+
+    const getOptionLabel = (option: string) => option;
+
+    const renderInput = (params: AutocompleteRenderInputParams) => (
+        <TextField  
+            sx={{
+                color: theme === "dark" ? "white" : "black",
+                '& .MuiInputBase-root': {
+                    borderRadius: '5px', 
+                    bgcolor: theme === "dark" ? "#3a3f467a" : "white", 
+                    color: theme === "dark" ? "white" : "black", 
+                },
+                '& .MuiInputBase-input': {
+                    color: theme === "dark" ? "white" : "black",
+                },
+                '& .MuiInputLabel-root': {
+                    color: theme === "dark" ? "white" : "black",
+                },
+                '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                        borderColor: theme === "dark" ? "white" : "black",
+                    },
+                    '&:hover fieldset': {
+                        borderColor: theme === "dark" ? "white" : "black",
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderColor: theme === "dark" ? "white" : "black",
+                    },
+                }
+            }}
+            {...params} label={<span className={` ${theme === "dark"? "text-white":"text-black"}`}> <SignpostIcon sx={{color: theme === "dark" ? "white" : "black"}}/> Адрес </span>} />
+    );
+
+    return (
+        <Autocomplete
+            sx={{ 
+                width: "100%",
+                border: 'none',
+                bgcolor: theme === "dark" ? "#3a3f467a" : "white",
+                color: theme === "dark" ? "white" : "black",
+                '& .MuiAutocomplete-popupIndicator': { 
+                    color: theme === "dark" ? "white" : "black",
+                }
+            }}
+            id="street-autocomplete"
+            options={filteblackProps.streets || []}
+            value={currentFilter.street ? currentFilter.street[0] : null}
+            onChange={handleChange}
+            inputValue={inputValue}
+            onInputChange={handleInputChange}
+            renderInput={renderInput}
+            filterOptions={filterOptions}
+            isOptionEqualToValue={(option, value) =>
+                option.toLowerCase() === (value || "").toLowerCase()
+            }
+            getOptionLabel={getOptionLabel}
+            noOptionsText={currentFilter.street && currentFilter.street.length === 0 ? "Напишите адрес" : "Нет вариантов"}
+        />
+    );
+}
---
+++ src/app/component/shortFilters/ShortFilters.tsx
@@ -0,0 +1,83 @@
+import { RootState } from "@/app/redux/store";
+import { useTheme } from "../provider/ThemeProvider";
+import { useSelector } from "react-redux";
+import {
+  allObjects,
+  FilteblackProps,
+  FilterUserOptions,
+} from "../../../../@types/dto";
+import { Dispatch, SetStateAction } from "react";
+import { ObjectIntrum } from "../../../../prisma";
+import React from "react";
+import { StreetSelectBig } from "../filterFields/adress/StreetSelectBig";
+
+type Props = {
+  objects: allObjects;
+  currentFilter: FilterUserOptions;
+  setCurrentFilter: Dispatch<SetStateAction<FilterUserOptions>>;
+  setFilteredHouse: Dispatch<SetStateAction<ObjectIntrum[]>>;
+  filteredHouse: ObjectIntrum[];
+  maxPrice: number;
+  minPrice: number;
+  setMinPrice: Dispatch<SetStateAction<number>>;
+  setMaxPrice: Dispatch<SetStateAction<number>>;
+  filteblackProps: FilteblackProps;
+  valueSliderPrice: [number, number];
+  setValueSliderPrice: Dispatch<SetStateAction<[number, number]>>;
+  countObjects: number;
+  resetPageAndReloadData: () => void;
+  isVisibleFilter: boolean;
+  setIsVisibleFilter: Dispatch<SetStateAction<boolean>>;
+};
+
+const filterRow = "flex w-full p-4 h-auto ";
+
+function ShortFilters({
+  filteblackProps,
+  objects,
+  currentFilter,
+  setCurrentFilter,
+  setFilteredHouse,
+  maxPrice,
+  minPrice,
+  setMinPrice,
+  setMaxPrice,
+  valueSliderPrice,
+  setValueSliderPrice,
+  countObjects,
+  resetPageAndReloadData,
+  isVisibleFilter,
+  setIsVisibleFilter,
+}: Props) {
+  const { favorites } = useSelector((state: RootState) => state.favorite);
+  const { theme } = useTheme();
+
+  const toggleVisibility = () => {
+    setIsVisibleFilter(!isVisibleFilter);
+  };
+
+  return (
+    <>
+      <section className="hidden md:flex flex-col w-full mt-[20px] items-center ">
+        <div className="flex flex-end w-[90%] ">
+          <StreetSelectBig
+            filteblackProps={filteblackProps}
+            currentFilter={currentFilter}
+            setCurrentFilter={setCurrentFilter}
+            resetPageAndReloadData={resetPageAndReloadData}
+          />
+        </div>
+        <div className="flex flex-end w-[90%]">
+          <button
+            onClick={toggleVisibility}
+            className=" bg-blue-500 text-white rounded"
+          >
+            {isVisibleFilter ? "Скрыть фильтры" : "Показать фильтры"}
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default React.memo(ShortFilters);
